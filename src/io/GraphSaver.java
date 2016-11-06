457b898de213fc1273333c46606eb4fcbf78f84b
MesutKoc <mesut.koc@tube-media.de>
2016-11-06 21:02:50 +0100

    saver fix

--- src/io/GraphSaver.java
+++ src/io/GraphSaver.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 
 /**
@@ -16,7 +18,6 @@ import org.graphstream.graph.Graph;
  * @since 2016-11-01
  */
 public class GraphSaver {
-	private static GraphReader reader1 = new GraphReader();
 	/**
 	 * 
 	 */
@@ -36,9 +37,20 @@ public class GraphSaver {
 	
 		Path path = Paths.get(file.toURI());
 		try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-			for (String strLines : reader1.getStrLines())
-				writer.write("" + strLines + ";"
-						+ System.getProperty("line.separator"));
+			for(Edge edge : graph.getEachEdge()) {
+				writer.write(convertsGraph(edge, false, edge.getAttribute("weight") != null ? true : true));
+				writer.newLine();
+			}
+//			for (String strLines : reader1.getStrLines())
+//				writer.write("" + strLines + ";"
+//						+ System.getProperty("line.separator"));
 		}
+	}
+	
+	public static String convertsGraph(Edge edge, Boolean showName, Boolean showWeight){
+		String name = showName ? " (" + edge.getId() + ")" : "",
+			   conn = edge.isDirected() ? "-->" : "--",
+			   weight = edge.getAttribute("weight") != null ? " : " + edge.getAttribute("weight") : "";
+		return edge.getNode0() + conn + edge.getNode1() + name + weight + ";";
 	}
 }
--- src/startup/Run.java
+++ src/startup/Run.java
@@ -2,6 +2,8 @@ package startup;
 
 import java.io.File;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
+
 import algorithmen.BreadthFirstSearch;
 import io.GraphReader;
 import io.GraphSaver;
@@ -34,16 +36,34 @@ public class Run {
 			GraphReader reader = new GraphReader();
 			
 			// Erstelle Graphen
-			Graph g = reader.openFile(new File("bspGraphen/graph01.gka"));
+//			Graph g = reader.openFile(new File("bspGraphen/graph01.gka"));
+			Graph pentaCircle = new MultiGraph("pentaCircle");
+	        pentaCircle.addNode("a");
+	        pentaCircle.addNode("b");
+	        pentaCircle.addNode("c");
+	        pentaCircle.addNode("d");
+	        pentaCircle.addNode("e");
+
+	        pentaCircle.addEdge("ab", "a", "b");
+	        pentaCircle.addEdge("ac", "a", "c");
+	        pentaCircle.addEdge("ae", "a", "e");
+	        pentaCircle.addEdge("ad", "a", "d");
+	        pentaCircle.addEdge("bd", "b", "d");
+	        pentaCircle.addEdge("bc", "b", "c");
+	        pentaCircle.addEdge("be", "b", "e");
+	        pentaCircle.addEdge("ed", "e", "d");
+	        pentaCircle.addEdge("ec", "e", "c");
+	        pentaCircle.addEdge("cd", "c", "d");
+	        pentaCircle.display();
 			
 			// Speicher Graphen ab
-			//GraphSaver.saveGraph(g, new File("bspGraphen/saved/graph01.gka"));
+			GraphSaver.saveGraph(pentaCircle, new File("bspGraphen/saved/graph_new.gka"));
 			
 			// Starte BFS
-			BreadthFirstSearch bfs = new BreadthFirstSearch();
-			bfs.initB(g, g.getNode("a"), g.getNode("h"));
-			bfs.startSearchEngine().getShortestPath();
-			System.out.println(bfs);
+//			BreadthFirstSearch bfs = new BreadthFirstSearch();
+//			bfs.initB(g, g.getNode("a"), g.getNode("h"));
+//			bfs.startSearchEngine().getShortestPath();
+//			System.out.println(bfs);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
