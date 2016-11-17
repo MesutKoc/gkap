81209e00ca7f53f50a5727da928fc76518142a88
MesutKoc <mesut.koc@tube-media.de>
2016-11-17 11:26:14 +0100

    Neue Version von Dijkstra

--- algorithmen/Dijkstra.java
+++ algorithmen/Dijkstra.java
@@ -6,13 +6,19 @@ import org.graphstream.algorithm.Algorithm;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
-
+/**
+ * <h1>Dijkstra.java</h1> Diese Klasse implementiert den Algorithmus
+ *
+ * @author Mesut koc
+ * @version 1.4
+ * @since 2016-10-30
+ */
 public class Dijkstra implements Algorithm {
 	private Graph graph;
 	private Node source, target;
-	Queue<Node> allNodes = new LinkedList<Node>();
-	Queue<Node> _queueNode = new LinkedList<Node>();
-	private int zugriffe = 0;
+	private Queue<Node> allNodes = new LinkedList<Node>();
+	private Queue<Node> _queueNode = new LinkedList<Node>();
+	private int graphAcc = 0;
 	
 	@Override
 	public void compute() {
@@ -21,6 +27,8 @@ public class Dijkstra implements Algorithm {
 			_queueNode.clear();
 			_queueNode.addAll(allNodes);
 			Node currentNode = allNodes.remove();
+			// TODO getNodeWithMinDistance
+			//Node currentNode = getNodeWithMinDistance();
 			while (!allNodes.isEmpty())
 				if (currentNode.getNumber("Distance") > allNodes.element().getNumber("Distance"))
 					currentNode = allNodes.remove();
@@ -36,7 +44,7 @@ public class Dijkstra implements Algorithm {
 	public void init(Graph g) {
 		try {
 			setGraph(g);
-			setDestination(graph.getNode(0), graph.getNode(graph.getNodeCount()-1));
+			setDestination(graph.getNode(0), graph.getNode(graph.getNodeCount() - 1));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -48,13 +56,14 @@ public class Dijkstra implements Algorithm {
 	 */
 	private void calcNewDistance(Node currentNode) {
 		for (Edge edge : graph.getEachEdge()) {
-			zugriffe++;
+			graphAcc++;
 			if (edge.getSourceNode() == currentNode && allNodes.contains(edge.getTargetNode()))
 				updateDistance(currentNode, edge.getTargetNode(), edge);
 		}
 	}
 	
 	/**
+	 * Akutliaisiert die Distanzen
 	 * @param u 
 	 * @param v
 	 * @param e die Kante die gerade durchläuft wird
@@ -73,12 +82,11 @@ public class Dijkstra implements Algorithm {
 	 * @param node der Knoten, wo die Information angezeigt werden soll
 	 */
 	private void updateLabel(Node node) {
-        node.setAttribute("ui.label", node.getId()
-                + " | Dist: " + node.getNumber("Distance")
-                + " | OK: " + node.getAttribute("OK")
-                + " | Pred: " + node.getAttribute("Predecessor")
-        );
-    }
+		node.setAttribute("ui.label",
+				node.getId() + " | Dist: " + node.getNumber("Distance")
+						+ " | OK: " + node.getAttribute("OK") + " | Pred: "
+						+ node.getAttribute("Predecessor"));
+	}
 	
 	/**
 	 * Diese Funktion dient dazu, um die Attribute mit Startwerten zu belegen
@@ -89,12 +97,13 @@ public class Dijkstra implements Algorithm {
 	private Queue<Node> config() {
 		for(Node node : graph) {
 			if(node == source){
-				node.addAttribute("Distance", 0.0);
-				node.addAttribute("OK", true);
-				node.addAttribute("Predecessor", node);
+				node.addAttribute("Distance", 0.0); // endgueltige Länge zum Startknoten
+				node.addAttribute("OK", true);	// sich selbst OK
+				node.addAttribute("Predecessor", node); // Vorgänger self
 			} else {
-				node.addAttribute("Distance", Double.POSITIVE_INFINITY);
-				node.addAttribute("OK", false);
+				node.addAttribute("Distance", Double.POSITIVE_INFINITY); // Entfernung ist unendlich
+				node.addAttribute("OK", false); // Knoten noch nicht gesehen
+				node.addAttribute("Predecessor", 0); // Vorgaenger noch nicht ermittelt
 			}
 			allNodes.add(node);
 			updateLabel(node);
@@ -102,18 +111,21 @@ public class Dijkstra implements Algorithm {
 		
 		// Zeige alle Gewichtungen von dem Knoten im Graph an
 		for (Edge edge : graph.getEachEdge()) 
-            edge.addAttribute("ui.label", edge.getAttribute("weight").toString());
+			edge.addAttribute("ui.label", edge.getAttribute("weight").toString());
   
 		return allNodes;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return String.format(
 				"[%s] [von=%s], [nach=%s], [distance=%f], [zugriffe=%d]",
 				this.getClass(), source, target,
-				(double) graph.getNode(target.toString()).getAttribute("Distance"),
-				zugriffe);
+				(double) target.getNumber("Distance"),
+				graphAcc);
 	}
 	
 	/**
@@ -125,10 +137,12 @@ public class Dijkstra implements Algorithm {
 		if(!isWeighted(g)) throw new IllegalArgumentException();
 		this.graph = g;
 	}
-	
+
 	/**
+	 * Prüft, ob der Graph gewichtet ist oder ungewichtet
+	 * 
 	 * @param g
-	 * @return
+	 * @return true, wenn der Graph gerichtiet ist ansonsten false
 	 */
 	private Boolean isWeighted(Graph g) {
 		for (Edge edge : g.getEachEdge())
@@ -143,7 +157,10 @@ public class Dijkstra implements Algorithm {
 	 * @throws Exception
 	 */
 	public void setDestination(Node node, Node node2) throws Exception {
-		if(node == null || node2 == null) throw new Exception("Ungültige Knoten");
+		if (node == null || node2 == null) throw new Exception("Ungültige Knoten"); // Ungültige Knoten
+		for (Edge e : graph.getEachEdge())
+			if (e.getNumber("weight") < 0)
+				throw new Exception("Negative Kantengewichte nicht erlaubt!"); // Ungültiges Kantengewicht
         this.source = node;
         this.target = node2;
 	}
