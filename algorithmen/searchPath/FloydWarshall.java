3adc6d68e9d1665911e21bbd8de55b5a39e39006
MesutKoc <mesut.koc@tube-media.de>
2016-11-25 17:08:46 +0100

    Floyd verbesserungen

--- algorithmen/searchPath/FloydWarshall.java
+++ algorithmen/searchPath/FloydWarshall.java
@@ -6,8 +6,8 @@ import org.graphstream.graph.Node;
 
 import java.util.*;
 
-import static java.util.Collections.reverse;
-import static java.util.Objects.*;
+import static java.lang.Double.parseDouble;
+import static java.util.Objects.requireNonNull;
 
 /**
  * @author Mesut
@@ -17,34 +17,32 @@ public class FloydWarshall implements Algorithm {
 	private Graph graph;
 	private List<Node> nodes;
     private Node source, target;
-    //	private int[][] distanceMatrix; // distanceMatrix[v][w] = die Länge der
-//									// kürzesten Paths von v->w.
-//									// distanceMatrix[v][w] ist INF, wenn es
-//									// keine Kante von u nach w gibt.
-//	private int[][] transitsMatrix;
     private Map<Node, Map<Node, Node>> predecessorMap;
-    private Map<Node, Map<Node, Integer>> costMap;
+    private Map<Node, Map<Node, Double>> costMap; // distanceMatrix[v][w] = die Länge der
+    // kürzesten Paths von v->w.
+    // distanceMatrix[v][w] ist INF, wenn es
+    // keine Kante von u nach w gibt.
 
 	/* (non-Javadoc)
 	 * @see org.graphstream.algorithm.Algorithm#compute()
 	 */
 	@Override
 	public void compute() {
-        for (int j = 0; j < nodes.size(); j++) {
-            for (int i = 0; i < nodes.size(); i++) {
-                for (int k = 0; k < nodes.size(); k++) {
+        for (int j = 0; j < nodes.size(); j++) { // Zeilen
+            for (int i = 0; i < nodes.size(); i++) { // Spalten
+                for (int k = 0; k < nodes.size(); k++) { // Schritte
                     if (i != j && j != k) {
-                        Integer dik = costMap.get(nodes.get(i)).get(nodes.get(k));
-                        Integer dij = costMap.get(nodes.get(i)).get(nodes.get(j));
-                        Integer djk = costMap.get(nodes.get(j)).get(nodes.get(k));
+                        Double dik = costMap.get(nodes.get(i)).get(nodes.get(k));
+                        Double dij = costMap.get(nodes.get(i)).get(nodes.get(j));
+                        Double djk = costMap.get(nodes.get(j)).get(nodes.get(k));
                         if (dij != null && djk != null && Math.min(dik, dij + djk) != dik) {
                             costMap.get(nodes.get(i)).replace(nodes.get(k), dij + djk);
                             predecessorMap.get(nodes.get(i)).replace(nodes.get(k), nodes.get(j));
                         }
                     }
                 }
-                Integer dii = costMap.get(nodes.get(i)).get(nodes.get(i));
-                if (dii != null && dii < 0) throw new IllegalArgumentException("Graph has negative circles");
+                Double dii = costMap.get(nodes.get(i)).get(nodes.get(i));
+                if (dii != null && dii < 0) return; // negative Kantenlänge
             }
         }
     }
@@ -52,57 +50,33 @@ public class FloydWarshall implements Algorithm {
 	/* (non-Javadoc)
 	 * @see org.graphstream.algorithm.Algorithm#init(org.graphstream.graph.Graph)
 	 */
-	@Override
-	public void init(Graph g) {
+    @Override
+    public void init(Graph g) {
         this.graph = Objects.requireNonNull(g);
-
         predecessorMap = new HashMap<>();
         costMap = new HashMap<>();
         nodes = new ArrayList<>();
-
-        for (Node e : graph.getEachNode()) nodes.add(e);
+        for (Node n : graph.getEachNode()) nodes.add(n);
 
         Iterator<Node> nodesForI = graph.getNodeIterator();
         for (int i = 0; i < nodes.size(); i++) {
-            Map<Node, Integer> tempCost = new HashMap<>();
+            Map<Node, Double> tempCost = new HashMap<>();
             Map<Node, Node> tempPred = new HashMap<>();
             Iterator<Node> nodesForJ = graph.getNodeIterator();
             Node nodeI = nodesForI.next();
+
             for (int j = 0; j < nodes.size(); j++) {
                 Node nodeJ = nodesForJ.next();
-                if (nodeI == nodeJ) {
-                    tempCost.put(nodes.get(j), 0);
+                if (nodeI.equals(nodeJ)) {
+                    tempCost.put(nodes.get(j), 0.0);
                     tempPred.put(nodes.get(j), null);
-                } else {
-                    if (nodeI.hasEdgeBetween(nodeJ)) {
-
-                    }
-                }
+                } else
+                    tempCost.put(nodes.get(j), nodeI.hasEdgeBetween(nodeJ) ? parseDouble(nodeI.getEdgeBetween(nodeJ).getAttribute("weight").toString()) : Double.POSITIVE_INFINITY);
             }
+            costMap.put(nodes.get(i), tempCost);
+            predecessorMap.put(nodes.get(i), tempPred);
         }
-//        for (int i = 0; i < nodes.size(); i++) {
-//            Map<Node, Integer> tempCost = new HashMap<>();
-//            Map<Node, Node> tempPred = new HashMap<>();
-//
-//            for (int j = 0; j < nodes.size(); j++) {
-//                if (i == j) {
-//                    tempCost.put(nodes.get(j), 0);
-//                    tempPred.put(nodes.get(j), null);
-//                } else {
-//                    for(Edge edge : graph.getEachEdge()) {
-//                        if(source.hasEdgeBetween(edge.getTargetNode()))
-//                            tempCost.put(nodes.get(j), parseInt(edge.getAttribute("weight").toString()));
-//                        else
-//                            tempCost.put(nodes.get(j), Integer.MAX_VALUE);
-//                    }
-//                }
-//
-//            }
-//            costMap.put(nodes.get(i), tempCost);
-//            predecessorMap.put(nodes.get(i), tempPred);
-//        }
     }
-
 
     public List<Node> getShortestPath(Node source, Node target) throws Exception {
         this.source = requireNonNull(source);
@@ -111,22 +85,9 @@ public class FloydWarshall implements Algorithm {
         return getShortestPath(target);
     }
 
-    private List<Node> getShortestPath(Node target) {
-        LinkedList<Node> path = new LinkedList<>();
-        Node step = target;
-
-        if (isNull(predecessorMap.get(step))) return null;
-        path.add(step);
-
-
-        while (nonNull(predecessorMap.get(step))) {
-            System.out.println(predecessorMap.get(source).get(step));
-            step = predecessorMap.get(source).get(step);
-            path.add(step);
-        }
-
-        reverse(path);
-        return path;
+    private ArrayList<Node> getShortestPath(Node target) {
+        // TODO rekursiv?
+        return null;
     }
 
     @Override
