6d28f5376203182625e39ecfd429cbcf9decbe24
MesutKoc <mesut.koc@tube-media.de>
2016-12-01 17:07:17 +0100

    maxFlow update

--- algorithm/optimizedFlow/MaxFlow.java
+++ algorithm/optimizedFlow/MaxFlow.java
@@ -1,7 +1,14 @@
 package algorithm.optimizedFlow;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * <h1>MaxFlow.java</h1> Diese Klasse implementiert beide Algorithmen
@@ -22,6 +29,34 @@ public class MaxFlow {
      * @return maxFlow result je nach Algorithmus
      */
     public static int findMaxFlow(Graph g, Node source, Node target, BY variant) {
+        requireNonNull(g);
+        requireNonNull(source);
+        requireNonNull(target);
+        requireNonNull(variant);
+
+        // Nodes buffern
+        Collection<Node> nodes = g.getNodeSet();
+        List<Node> nodeList = new ArrayList<>(nodes);
+        // Edges buffern
+        Collection<Edge> edges = g.getEdgeSet();
+        List<Edge> edgeList = new ArrayList<>(edges);
+
+        // 1. Schritt: die Initialisierung
+        // Der Fluss wird auf 0 gesetzt
+        for (int indexEdges = 0; indexEdges < edgeList.size(); indexEdges = indexEdges + 2)
+            g.setAttribute("fluss", 0, edgeList.get(indexEdges), edgeList.get(indexEdges + 1));
+
+        // Jede Ecke wird als nicht inspiziert (0) und nicht markiert (0) markiert
+        for (Node currentNode : nodeList) {
+            g.setAttribute("inspiziert", 0, currentNode);
+            g.setAttribute("markiert", 0, currentNode);
+        }
+
+        // Die Quelle markieren
+        g.setAttribute("markiert", source, 1);
+        g.setAttribute("maxFlow", source, Integer.MAX_VALUE);
+
+        // TODO Schritt 2 Inspektion und Markierung
         // TODO findMaxFlow
         return 0;
     }
